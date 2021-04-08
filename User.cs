aa67609e1a3cc1140763d7ace67727824d152ad0
AoYangJia <1357878465@qq.com>
2021-04-08 14:46:18 +0800

    添加项目文件。

---
+++ User.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2_Server_Gui
+{
+    class User
+    {
+        private string user_Name;
+        private int user_ID;
+        private string user_Password;
+        public User(string name,int id,string password)
+        {
+            user_Name = name;
+            user_ID = id;
+            user_Password = password;
+        } 
+
+        public void set_Name(string name)
+        {
+            user_Name = name;
+        }
+
+        public string get_Name()
+        {
+            return user_Name;
+        }
+
+        public void set_Password(string new_Password)
+        {
+            user_Password = new_Password;
+        }
+
+        public bool is_Password(string password)
+        {
+            return string.Equals(user_Password, password);
+        }
+
+    }
+}
